9ce956919a43a5df3d24a08cf424214eb8a020a9
noumanTunda <ntunda4000@gmail.com>
2025-08-19 16:17:36 +0300

    Update settings Activity

--- app/src/main/java/com/group/campus/SettingsActivity.java
+++ app/src/main/java/com/group/campus/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.group.campus;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,19 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settingsActivity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton backButton = findViewById(R.id.settings_back_arrow);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
