59202dbe7752e5bf64e290251f8cfe706096aa38
noumanTunda <ntunda4000@gmail.com>
2025-08-22 14:56:40 +0300

    Refactor SettingsActivity and layouts
    - Update text in `fragment_change_password.xml`
    - Modify `activity_settings.xml`:
        - Change back button from `ImageButton` to `MaterialButton`
        - Increase `CardView` corner radius
        - Remove "Manage Devices", "Data Policy", "Help & Support" and most "About" section items
    - Update `SettingsActivity.java`:
        - Change back button type to `MaterialButton`
        - Remove listeners for deleted items

--- app/src/main/java/com/group/campus/SettingsActivity.java
+++ app/src/main/java/com/group/campus/SettingsActivity.java
@@ -13,9 +13,12 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.button.MaterialButton;
 import com.group.campus.fragments.ChangePasswordFragment;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private MaterialButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,7 @@ public class SettingsActivity extends AppCompatActivity {
             return insets;
         });
 
-        ImageButton backButton = findViewById(R.id.settings_back_arrow);
+        backButton = findViewById(R.id.settings_back_arrow);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,14 +45,6 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 launchChangePasswordFragment();
-            }
-        });
-
-        View manageDevicesItem = findViewById(R.id.item_manage_devices);
-        manageDevicesItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "Manage Devices was clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -66,62 +61,6 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(SettingsActivity.this, "Language was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View dataPolicyItem = findViewById(R.id.item_data_policy);
-        dataPolicyItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "Data Policy was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View faqItem = findViewById(R.id.item_faq);
-        faqItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "Frequently Asked Questions was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View contactSupportItem = findViewById(R.id.item_contact_support);
-        contactSupportItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "Contact Support was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View reportBugItem = findViewById(R.id.item_report_bug);
-        reportBugItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "Report Bug was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View sendFeedbackItem = findViewById(R.id.item_send_feedback);
-        sendFeedbackItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "Send Feedback was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View licenseItem = findViewById(R.id.item_licenses);
-        licenseItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "License was clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View acknowledgementsItem = findViewById(R.id.item_acknowledgements);
-        acknowledgementsItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SettingsActivity.this, "Acknowledgments was clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
